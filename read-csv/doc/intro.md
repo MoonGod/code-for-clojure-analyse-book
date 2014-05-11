5768f8e2036e04b5ec683c4b4727be152604dd06
MoonGod <shenpengzhang@163.com>
2014-05-11 22:38:55 +0800

    read dataset from csv file

---
+++ read-csv/doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to read-csv
+
+TODO: write [great documentation](http://jacobian.org/writing/great-documentation/what-to-write/)
---
+++ read-csv/test/read_csv/core_test.clj
@@ -0,0 +1,7 @@
+(ns read-csv.core-test
+  (:require [clojure.test :refer :all]
+            [read-csv.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
